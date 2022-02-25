3a48f8204b7aa25673777129ef722876d0d82697
salxvador <sal.salvador@gmail.com>
2022-02-25 14:29:29 -0500

    added abstraction assignment

---
+++ ABC_Abstraction.py
@@ -0,0 +1,34 @@
+
+from abc import ABC, abstractmethod
+
+
+##Your class should contain at least one abstract method and one regular method.  
+##Create a child class that defines the implementation of its parents abstract method.
+##Create an object that utilizes both the parent and child methods.
+##Add comments throughout your Python explaining your code.
+
+
+##Parent abstract Class:
+class Animal(ABC):
+    #regular method move():
+    def move(self):
+        print("I am crawling around.")
+
+    #abstract method speak() requires decorator '@abstractmethod'
+    @abstractmethod
+    def speak(self):
+        pass
+
+##Child Class:
+class Dog(Animal):
+    #must define abstract method that we passed on in the parent:
+    def speak(self):
+        print("I can bark and growl.")
+
+#instantiate object of child class:
+spot = Dog()
+#call the regular method defined in parent:
+spot.move()
+#call the abstract method only defined in child class:
+spot.speak()
+ 
